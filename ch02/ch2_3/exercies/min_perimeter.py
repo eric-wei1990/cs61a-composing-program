ab6b8a5a1d7f34f11f49bc4fe11348f90097a5c3
Eric Wei <eric-wei1990@users.noreply.github.com>
2021-09-13 14:49:38 +0000

    Add minimum perimeter example

---
+++ ch02/ch2_3/exercies/min_perimeter.py
@@ -0,0 +1,17 @@
+def width(area, height):
+    return area // height
+
+def perimeter(width, height):
+    return 2 * width + 2 * height
+
+def divisions(n):
+    return [1] + [x for x in range(2, n) if n % x == 0]
+
+def min_perimeter(area):
+    heights = divisions(area)
+    print(heights)
+    perimeters = [perimeter(width(area, h), h) for h in heights]
+    
+    return min(perimeters)
+
+print(min_perimeter(1))
